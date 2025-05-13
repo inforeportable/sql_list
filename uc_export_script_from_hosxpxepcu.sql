7f30eb851f097af7018fb58db2d07f845a41431f
inforeportable <37160470+inforeportable@users.noreply.github.com>
2025-05-13 10:47:58 +0700

    Update uc_export_script_from_hosxpxepcu.sql
    [2025-05-13 10:47:30]

--- uc_export_script_from_hosxpxepcu.sql
+++ uc_export_script_from_hosxpxepcu.sql
@@ -1,12 +1,12 @@
--- uc_export_script_from_hosxpxepcu [2025-04-08 13:33:18]
--- update [2025-04-08 13:33:18]
+-- uc_export_script_from_hosxpxepcu [2025-05-13 10:47:30]
+-- update [2025-05-13 10:47:30]
 -- เพื่อประมวลผลข้อมูลการให้บริการเพื่อนำมาใช้ในการติดตามเบื้องต้น
 
-set @s_date := '2025-03-01' ;
-set @e_date := '2025-03-31' ;
+set @s_date := '2025-04-01' ;
+set @e_date := '2025-04-30' ;
 set @hospital_code := (select opdconfig.hospitalcode from opdconfig);
 set @dbversion := (select version()) ;
-set @sqlversion:= '[2025-04-08 13:33:18]' ;
+set @sqlversion:= '[2025-05-13 10:47:30]' ;
 
 select
 -- service data
